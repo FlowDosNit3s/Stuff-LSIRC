2c7b58af5533452f0cfa8b85ec6d206b2cd6cc15
FlowDosNit3s <diogocosta.dc3@gmail.com>
2026-02-10 18:36:52 +0000

    Add exercicios IntelliJ project and Main.java
    Initial commit for the 'exercicios' exercise folder: add .gitignore to exclude IDE/build/OS files, IntelliJ .idea configuration files, module descriptor (exercicios.iml), and src/Main.java. Main.java contains a small demo printing char and int variables and examples of concatenation and arithmetic to illustrate basic Java output.

---
+++ PP/FICHAS/1/exercicios/src/Main.java
@@ -0,0 +1,21 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        /**
+         * Imprimimos a duas variaveis char, ou seja, variaveis que armazenam um unico caractere (char l = 'l' , p = 'p';) :
+         * -Resultado obtivemos os dois a serem imprimidos como l para o primeiro e p para o segundo.
+         * Imprimimos mais duas variaveis int, ou seja, variaveis que armazenam números inteiros positivos ou negativos, sem casas decimais (int q = 4 , d = 2;) :
+         * -
+         */
+        char l = 'l' , p = 'p';
+        int q = 4 , d = 2;
+        System.out.println(l);
+        System.out.println(p);
+        System.out.println(d);
+        System.out.println(q);
+        System.out.println(l + p + 2);
+        System.out.println("" + l + p + 2);
+        System.out.println(q + d);
+    }
+}
