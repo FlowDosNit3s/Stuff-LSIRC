b68cb4bfe295b6b369e11f12959a98bd882dfe7d
FlowDosNit3s <diogocosta.dc3@gmail.com>
2026-03-02 10:20:56 +0000

    Add PAW aula 1 HTML and JS examples
    Add initial PAW 'Aula 1' example files. Creates index.html (loads main.js) and two JS example files: 1.1/main.js logs "Hello World", and 1.2/main.js contains console.log demonstrations of equality (== vs ===), type coercion, array identity/reference behavior, and arithmetic/modulo examples for teaching purposes.

---
+++ 2ºano/2ºsemestre/PAW/fichas/2/1.2/main.js
@@ -0,0 +1,21 @@
+console.log(1 == "1");
+console.log(1 === "1");
+console.log("a" == 97);
+console.log(1/3);
+//console.log(variavel);
+
+var x = 12;
+console.log(x);
+
+console.log([] == []);
+console.log([1] == [1]);
+
+var a = [1,2];
+var b = a;
+console.log(a == b);
+console.log(a === b);
+
+console.log(1 != 0);
+!console.log(1 !=5);
+console.log(11% 2);
+console.log(11/2);
